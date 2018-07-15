ce7332942cf42d2d21a101d5da5b719ff5d2612f
Yolotzin Dunbar <yltzndnbr@gmail.com>
2018-07-15 14:56:11 -0700

    Testing git commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
