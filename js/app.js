7b30e3e351ea369f2925fcf8f7666aceb3d01b04
Yolotzin Dunbar <yltzndnbr@gmail.com>
2018-07-17 21:51:08 -0700

    Uncommented Player render function and set Player starting location

--- js/app.js
+++ js/app.js
@@ -8,13 +8,9 @@ const Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = 0;
-    this.y = 0;
+    this.y = 55;
     this.step = 101;
 };
-
-const bug1 = new Enemy();
-const allEnemies = [];
-allEnemies.push(bug1);
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -38,13 +34,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class Hero {
+    constructor() {
+        this.step = 101;
+        this.x = this.step * 2;
+        this.y = this.step * 4;
+        this.sprite = 'images/char-boy.png';
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+const bug1 = new Enemy();
+const allEnemies = [];
+allEnemies.push(bug1);
+const player = new Hero();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -152,8 +152,7 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
-        //player.render();
+        player.render();
     }
 
     /* This function does nothing but it could have been a good place to
